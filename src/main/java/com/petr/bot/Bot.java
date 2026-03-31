579edb6f8848463f72e532a5b2f53e4cabe46b37
CapibaraLover <merzlikin.0404@mail.ru>
2026-03-31 11:13:43 +0300

    ADD определил основные хендлеры бота

--- src/main/java/com/petr/bot/Bot.java
+++ src/main/java/com/petr/bot/Bot.java
@@ -1,6 +1,12 @@
 package com.petr.bot;
 
+import com.petr.bot.filters.isAdmin;
+import com.petr.bot.filters.userHasConfig;
 import io.github.natanimn.telebof.BotClient;
+import io.github.natanimn.telebof.BotContext;
+import io.github.natanimn.telebof.annotations.MessageHandler;
+import io.github.natanimn.telebof.enums.MessageType;
+import io.github.natanimn.telebof.types.updates.Message;
 
 public class Bot {
     final private BotClient bot;
@@ -20,6 +26,96 @@ public class Bot {
     }
 
     class BotHandler{
-        // TODO написать handler
+        @MessageHandler(
+                commands = "start"
+        )
+        void onStart(BotContext bot, Message message){
+            bot.sendMessage(message.chat.id, """
+                Привет! Зарегистрировал тебя.
+                
+                Этот бот - менеджер твоих конфигов.
+                Если не помнишь как им пользоваться - введи команду /help
+                """).exec();
+            //TODO добавление пользователя в бд здесь
+        }
+
+        @MessageHandler(
+                commands = "meow"
+        )
+        void onMeow(BotContext bot, Message message){
+            bot.sendMessage(message.chat.id, """
+                meow 🐈
+                """).exec();
+        }
+
+        //TODO обновить по ходу разработки
+        @MessageHandler(
+                commands = "help"
+        )
+        void onHelp(BotContext bot, Message message){
+            bot.sendMessage(message.chat.id, """
+                Команды бота:
+                /help - Информационное сообщение
+                /get_config - Создать новый или получить свой конфиг
+                """).exec();
+        }
+
+        //TODO написать фильтр и хэндлер получения конфига из бд
+        @MessageHandler(
+                priority = 1,
+                commands = "get_config",
+                filter = userHasConfig.class
+        )
+        void userHasConfig(BotContext bot, Message message){
+
+        }
+
+        //TODO написать хэндлер создания конфига
+        @MessageHandler(
+                priority = 2,
+                commands = "get_config"
+        )
+        void createConfig(BotContext bot, Message message){
+
+        }
+
+        @MessageHandler(
+                filter = isAdmin.class,
+                commands = "admin"
+        )
+        void onAdmin(BotContext bot, Message message){
+            bot.setState(message.chat.id, "admin_state");
+            bot.sendMessage(message.chat.id, """
+                Добро пожаловать, Админ!
+                Список админских команд:
+                /admin - войти в админ режим
+                
+                /delConfig - удалить конфиг по юзернейму
+                
+                /exAdmin - выйти из админ режима
+                """).exec(); //TODO изменить по ходу разработки
+        }
+
+        @MessageHandler(
+                filter = isAdmin.class,
+                state = "admin_state",
+                commands = "exAdmin"
+        )
+        void onExitAdmin(BotContext bot, Message message){
+            bot.clearState(message.chat.id);
+            bot.sendMessage(message.chat.id, """
+                Вы вышли из админ режима!
+                """).exec();
+        }
+
+        //FIXME если не работает - изменить приоритет сообщения
+        @MessageHandler(
+                type = MessageType.TEXT
+        )
+        void onDefault(BotContext context, Message message){
+            context.sendMessage(message.chat.id, """
+                Я вас не понял, напишите /help чтобы увидеть доступные команды.
+                """).exec();
+        }
     }
 }
